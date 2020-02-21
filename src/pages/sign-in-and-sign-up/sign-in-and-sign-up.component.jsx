b968da237f67d9947901c9d9d678a9b001cf39ad
Riventus <riventus@aksaramaya.com>
2020-02-21 16:29:38 +0700

    add pages sign in n sign up

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+// SignInAndSignUpPage
+export default () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn/>
+    </div>
+  );
+}
