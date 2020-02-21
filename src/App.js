6cf0b953a24ac91289e88c73d0a658d6f35a2245
Riventus <riventus@aksaramaya.com>
2020-02-21 16:28:51 +0700

    update app.js for sign in component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
       <Header/>
-      {/* <Switch> */}
+      <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-      {/* </Switch> */}
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
