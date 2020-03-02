27a12b1ce617307aa972fcc44de48abf029a545d
Riventus <riventus@aksaramaya.com>
2020-03-02 21:06:49 +0700

    implemented cart reducer on cart component in header

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/11.2 shopping-bag.svg.svg';
 import './cart-icon.styles.scss';
+import { connect } from 'react-redux';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = () => {
+const CartIcon = ({ toggleCartHidden }) => {
   return(
-  <div className="cart-icon">
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
   );
 }
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null,mapDispatchToProps)(CartIcon);
