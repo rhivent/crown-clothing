2ae97c1ab6c30929271d0f1e97b6bce20ae09b07
Riventus <riventus@aksaramaya.com>
2020-04-11 13:09:03 +0700

    folder 9 : vid 2 =>> add redux-persist for maintaining cart on storage

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import store from './redux/store';
+import {store,persistor} from './redux/store';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
