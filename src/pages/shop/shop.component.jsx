f432aaebf436546764c68e090f64c696e698fd25
Riventus <riventus@aksaramaya.com>
2020-04-26 15:43:00 +0700

    folder 16 : video 9 ==> adding shop adta to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import CollectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
 import { Route } from 'react-router-dom';
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import {updateCollections} from '../../redux/shop/shop.actions';
+import { connect } from 'react-redux';
 
-const ShopPage = ({match,collections}) => {
+const ShopPage = ({match,updateCollections}) => {
+  const unsubscribeFromSnapshot = null;
+
+  React.useEffect(() =>{
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  },[]);
+
   return (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverviewComponent}/>
@@ -12,4 +26,8 @@ const ShopPage = ({match,collections}) => {
   );
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections : collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null,mapDispatchToProps)(ShopPage);
