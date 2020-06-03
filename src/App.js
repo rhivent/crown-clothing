910c042313536cc966f89a7d0cadb1dcf15ae4a6
Riventus <riventus@aksaramaya.com>
2020-06-03 20:02:16 +0700

    update redux saga for sign with email and refractor function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import { Switch,Route,Redirect } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -10,12 +11,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component.jsx';
 import Header from './components/header/header.component';
 
-import { auth,createUserProfileDocument } from './firebase/firebase.utils';
-
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component{
   // constructor(){
@@ -29,7 +25,7 @@ class App extends React.Component{
 
   componentDidMount(){
     // collectionsArray
-    const {setCurrentUser} = this.props;
+    // const {setCurrentUser} = this.props;
     // this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
     //   if(userAuth){
     //     const userRef = await createUserProfileDocument(userAuth);
@@ -72,45 +68,9 @@ class App extends React.Component{
   }
 }
 
-// function App() {
-//   const [currentUser,setcurrentUser] = React.useState(null);
-//   let unsubscribeFromAuth = null;
-//   const unsubscribeFromAuthData = React.useRef();
-
-//   React.useEffect(()=>{
-//     unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-//       setcurrentUser({user});
-
-//       console.log(user);
-//     })
-//     // unsubscribeFromAuthData.current = unsubscribeFromAuth();
-//   },[]);
-
-//   React.useEffect(() => {
-//     return () => unsubscribeFromAuth();
-//   },[unsubscribeFromAuth]);
-
-//   console.log(unsubscribeFromAuth);
-
-//   return (
-//     <div>
-//       <Header currentUser={currentUser} />
-//       <Switch>
-//         <Route exact path='/' component={HomePage} />
-//         <Route path='/shop' component={ShopPage} />
-//         <Route path='/signin' component={SignInAndSignUpPage} />
-//       </Switch>
-//     </div>
-//   );
-// }
-
 const mapStateToProps = createStructuredSelector({
   currentUser : selectCurrentUser,
   // collectionsArray : selectCollectionsForPreview
-})
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser : user => dispatch(setCurrentUser(user))
 });
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -12,14 +12,12 @@ const userReducer = (state = INITIAL_STATE,action) => {
         currentUser:action.payload,
         error : null
       }
-    case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-    case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+    case UserActionTypes.SIGN_IN_SUCCESS:
       return{
         ...state,
         currentUser:action.payload
       }
-    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_IN_FAILURE:
       return{
         ...state,
         error:action.payload
